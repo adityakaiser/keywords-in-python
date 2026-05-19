77510c205e03e21abb4bc4ff282dec05499e2dd4
adityakaiser <singhadityakaiser@gmail.com>
2026-05-19 21:49:59 +0530

    Add files via upload

---
+++ extra.py
@@ -0,0 +1,10 @@
+
+def add():
+    a=int(input("enter a number:"))
+    b=int(input("enter second number:"))
+    return a+b
+
+
+
+
+print(add())
---
+++ file1.py
@@ -0,0 +1,11 @@
+a=(input("enter your word:"))
+for i in a:
+    if i=='A':
+        
+        
+        print("A is found") 
+
+        break
+
+    else:
+         print("A is not found found")
